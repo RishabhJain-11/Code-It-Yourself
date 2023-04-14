e5547565b59711083998a93241821454026a5f70
Rishabh Jain <90708164+RishabhJain-11@users.noreply.github.com>
2023-04-14 23:45:09 +0530

    14-04-2023

--- thapa_mern/src/index.js
+++ thapa_mern/src/index.js
@@ -8,7 +8,9 @@ mongoose.connect("mongodb://127.0.0.1:27017/ttchannel", { useNewUrlParser: true,
 const playlistSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: true,
+        unique: true,
+        lowercase: true,
     },
     ctype: String,
     videos: Number,
@@ -33,65 +35,70 @@ const createDocument = async () => {
             author: "Thapa Technical",
             active: true,
         });
-        const mongoPlaylist = new Playlist({
-            name: "Mongo DB",
-            ctype: "Back End",
-            videos: 10,
-            author: "Thapa Technical",
-            active: true,
-        });
-        const nextPlaylist = new Playlist({
-            name: "Next JS",
-            ctype: "Front End",
-            videos: 20,
-            author: "Thapa Technical",
-            active: true,
-        });
-        const emailjsPlaylist = new Playlist({
-            name: "Email JS",
-            ctype: "Front End",
-            videos: 5,
-            author: "Thapa Technical",
-            active: true,
-        });
-        const res = await Playlist.insertMany([emailjsPlaylist, nextPlaylist, mongoPlaylist, reactPlaylist]);
+        // const mongoPlaylist = new Playlist({
+        //     name: "Mongo DB",
+        //     ctype: "Back End",
+        //     videos: 10,
+        //     author: "Thapa Technical",
+        //     active: true,
+        // });
+        // const nextPlaylist = new Playlist({
+        //     name: "Next JS",
+        //     ctype: "Front End",
+        //     videos: 20,
+        //     author: "Thapa Technical",
+        //     active: true,
+        // });
+        // const emailjsPlaylist = new Playlist({
+        //     name: "Email JS",
+        //     ctype: "Front End",
+        //     videos: 5,
+        //     author: "Thapa Technical",
+        //     active: true,
+        // });
+        const res = await Playlist.insertMany([reactPlaylist]);
         console.log(res);
     } catch (error) {
-        console.log("Error aara hai")
+        console.log("Error aara hai", error)
     }
 }
-// createDocument();
+// emailjsPlaylist
+// nextPlaylist, mongoPlaylist, 
 
-// const getDocument = async () => {
-//     const res = await Playlist
-//         .find({ctype: "Databse"})
-//         .select({ name: 1 })
-//         .countDocuments();
-//     console.log(res);
-// }
+createDocument();
+
+
+
+const getDocument = async () => {
+    const res = await Playlist
+        .find({ctype: "Databse"})
+        .select({ name: 1 })
+        .countDocuments();
+    console.log(res);
+}
 // getDocument();
 
-// const updateDocument = async (_id) => {
-//     try {
-//         const res = await Playlist.findByIdAndUpdate({ _id }, {
-//             $set: {
-//                 name: "Mongo DB"
-//             }
-//         });
-//     } catch (error) {
-//         console.log(error)
-//     }
-// }
+const updateDocument = async (_id) => {
+    try {
+        const res = await Playlist.findByIdAndUpdate({ _id }, {
+            $set: {
+                name: "Mongo DB"
+            }
+        });
+    } catch (error) {
+        console.log(error)
+    }
+}
 
 // updateDocument("64382df8d725a1a4828b34aa");
 
 const deleteDocument = async (_id) => {
     try {
         const res = await Playlist.findByIdAndDelete({ _id });
-        console.log("The deleted document is ",res);
+        console.log("The deleted document is ", res);
     } catch (error) {
         console.log("Error Homecoming")
     }
 }
 
-deleteDocument("64382df8d725a1a4828b34a9");
+// deleteDocument("64382df8d725a1a4828b34a9");
