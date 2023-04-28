bb4bcb05b123efcf3efd591ccc42f0ce31d33a31
Rishabh Jain <90708164+RishabhJain-11@users.noreply.github.com>
2023-04-28 17:43:55 +0530

    28-04-2023

---
+++ projects_april/firebase_tutorial/src/components/auth.js
@@ -0,0 +1,43 @@
+import { auth, googleProvider } from '../config/firebase';
+import { createUserWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
+import { useState } from 'react';
+
+export const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const signIn = async () => {
+        try {
+            await createUserWithEmailAndPassword(auth, email, password);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    const signInWithGoogle = async () => {
+        try {
+            await signInWithPopup(auth, email, password);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    const logout = async () => {
+        try {
+            await signOut(auth);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    return (
+        <div>
+            <input type="email" placeholder="email" onChange={(e) => setEmail(e.target.value)} />
+            <input type="password" placeholder="password" onChange={(e) => setPassword(e.target.value)} />
+            <button onClick={signIn}>Sign In</button>
+
+            <button onClick={signInWithGoogle}>Sign in with Google</button>
+            <button onClick={logout}>Logout</button>
+        </div>
+    )
+}
