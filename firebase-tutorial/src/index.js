ecb6ce1ded7358c712ef5258570936a5d6288a13
Rishabh Jain <90708164+RishabhJain-11@users.noreply.github.com>
2023-05-06 21:28:47 +0530

    06-05-2023

---
+++ firebase-tutorial/src/index.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDei3ixIL5wVxrtST1aCqLSxa94s7Y1H1k",
+    authDomain: "fir-9iners.firebaseapp.com",
+    projectId: "fir-9iners",
+    storageBucket: "fir-9iners.appspot.com",
+    messagingSenderId: "380072840009",
+    appId: "1:380072840009:web:d9feff579f46e49edce898"
+};
+
+initializeApp();
