30cf32fb535fb442cc45fd10e47c52f80f361cf5
Rishabh Jain <90708164+RishabhJain-11@users.noreply.github.com>
2023-12-31 17:24:34 +0530

    New Codes

---
+++ pythonprojects/ex.py
@@ -0,0 +1,47 @@
+# Mosaic Studios
+# Whisper Films
+# Echo Productions
+# Ether Studios
+# Spectra Pictures
+# Arcane Films
+# Quasar Films
+# Aurora Productions
+# Nexus Pictures
+# MultiVerse Studios
+
+# Anime
+# Animation Films
+# Films
+# Short Films
+# Drama
+# Web Series
+# Music
+
+
+# 1 - Punjab
+# Haryana
+# Himachal
+# Uttranchal
+# Delhi
+# Gilgit
+# Baltistan
+# Ladakh
+# Kashmir
+# Jammu
+# Rajasthan
+# 12 - UP
+
+# Bihar - 1.9cr
+# Jharkhand - 30 lakhs
+# CG - 20 lakhs
+# Odisha - 6 cr
+# West Bengal - 13 cr
+
+
+# Tyger Studios
+# Content Oriented Movies
+# No super stars left
+# All A-listers are now working in Dellywood
+# Focus is more on real life stories and events
+# Improvised VFX, BGM, Design Team.
+# Most of the stars are now coming from East Part of the country and Maharashtra
---
+++ pythonprojects/geeksemailtrack/geeksemailtrack/items.py
@@ -0,0 +1,21 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+import re
+
+from scrapy_selenium import SeleniumRequest
+
+from scrapy.linkextractors.lxmlhtml import LxmlLinkExtractor
+
+
+class GeeksemailtrackItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    pass
+
+
+
