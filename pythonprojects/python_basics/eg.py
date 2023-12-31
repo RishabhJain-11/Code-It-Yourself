66f3bce20b35bee76e06b9de67444a5717c41ac6
Rishabh Jain <90708164+RishabhJain-11@users.noreply.github.com>
2023-12-31 17:24:19 +0530

    New Codes

---
+++ pythonprojects/python_basics/eg.py
@@ -0,0 +1,88 @@
+# nums = []
+# nums.append(21)
+# nums.append(40.5)
+# nums.append("String")
+# print(nums)
+
+# def getInteger():
+#       result = int(input("Enter Integer: "))
+#       return result
+#
+# def Main():
+#       print("Started")
+#       output = getInteger()
+#       print(output)
+#
+# if __name__ == "__main__":
+#       Main()
+
+# import math
+#
+# def Main():
+#     num = -85
+#
+#     num = math.fabs(num)
+#     print(num)
+#
+# if __name__ == "__main__":
+#     Main()
+
+# var1 = 5
+# def some_func():
+#     var2 = 6
+#
+#     def some_inner_func():
+#         var3 = 7
+
+
+# count = 5
+# def some_method():
+#     global count
+#     count = count + 1
+#     print(count)
+# some_method()
+
+# s = 1 + 2 + 3 + \
+#     4 + 5 + 6 + \
+#     7 + 8 + 9
+#
+# n = (1 * 2 * 3 + 7 + 8 + 9)
+#
+# footballer = ['messi', 'neymar', 'suarez']
+# # Rishabh
+# x = {1 + 2 + 3}
+
+# i = 20
+# if(i < 15):
+#     print("i is smaller than 15")
+#     print("i'm in if block")
+# else:
+#     print("i is greater than 15")
+#     print("i'm in else block")
+#
+# print("i'm not in if and not in else block")
+
+# x, y = input("Enter 2 values: ").split()
+# print("Number of boys: ", x)
+# print("Number of girls: ", y)
+#
+# x, y, z = input("Enter 3 values: ").split()
+# print("Total no. of students", x)
+# print("No. of boys is : ", y)
+# print("No. of girls is : ", z)
+#
+# a,b = input("Enter 2 values: ").split()
+# print("First no. is {} and second no. is {}".format(a, b))
+#
+# x = list(map(int, input("Enter multiple values: ").split()))
+# print("List of student: ", x)
+
+# print("Welcome to ", end = '')
+# print("GeeksforGeeks", end = ' ')
+
+# print('G', 'F', sep = '', end = '')
+# print('G')
+# print('09', '12', '2016', sep = '-', end = '\n')
+# print('Red', 'Green', 'Blue', sep=', ', end='@')
+# print('geeksforgeeks')
+
