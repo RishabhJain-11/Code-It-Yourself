0f7528ac3a74c161aefdb859cdd8f6be08b29dee
Rishabh Jain <90708164+RishabhJain-11@users.noreply.github.com>
2024-01-02 23:26:05 +0530

    New Codes

--- pythonprojects/python_basics/rj.py
+++ pythonprojects/python_basics/rj.py
@@ -32,34 +32,130 @@
 
 # help(print)
 
-import random
-
-secret_number = random.randint(1, 500)
-print ("Pick a number between 1 to 500")
-while True:
-    res = input("Guess the number: ")
-    if res == secret_number:
-        print("You win")
-        break
-    else:
-        print("You Lose")
-        continue
-
-
+# import random
+#
+# secret_number = random.randint(1, 500)
+# print ("Pick a number between 1 to 500")
+# while True:
+#     res = input("Guess the number: ")
+#     if res == secret_number:
+#         print("You win")
+#         break
+#     else:
+#         print("You Lose")
+#         continue
 
 
 
 
+# class Dog:
+#     attr1 = "mammal"
+#
+#     def __init__(self, name):
+#         self.name = name
+#
+# Rodger = Dog("Rodger")
+# Tommy = Dog("Tommy")
+#
+# print("Rodger is a {}".format(Rodger.__class__.attr1))
+# print("Tommy is also a {}".format(Tommy.__class__.attr1))
+# print("His name is {}".format(Rodger.name))
+# print("And his name is {}".format(Tommy.name))
+
+# class Person(object):
+#     def __init__(self, name, idnumber):
+#         self.name = name
+#         self.idnumber = idnumber
+#
+#     def display(self):
+#         print(self.name)
+#         print(self.idnumber)
+#
+#     def details(self):
+#         print("my name is {}".format(self.name))
+#         print("id number: {}".format(self.idnumber))
+#
+# class Employee(Person):
+#     def __init__(self, name, idnumber, salary, post):
+#         self.salary = salary
+#         self.post = post
+#
+#         Person.__init__(self, name, idnumber)
+#
+#     def details(self):
+#         print("My name is {}".format(self.name))
+#         print("idno.: {}".format(self.idnumber))
+#         print("Post: {}".format(self.post))
+#
+# a = Employee('Rahul', 886012, 20000000, "Intern")
+#
+# a.display()
+# a.details()
+
+# class Bird:
+#     def intro(self):
+#         print("There are many types of birds")
+#
+#     def flight(self):
+#         print("Most of the birds can fly but some cannot")
+#
+# class Crow(Bird):
+#     def flight(self):
+#         print("Crows can fly")
+#
+# class Pigeon(Bird):
+#     def flight(self):
+#         print("Pigeon can fly")
+#
+# obj_bird = Bird()
+# obj_spr = Crow()
+# obj_pig = Pigeon()
+#
+# obj_bird.intro()
+# obj_bird.flight()
+#
+# obj_pig.intro()
+# obj_pig.flight()
+
+class Base:
+    def __init__(self):
+        self.a = "GeeksforGeeks"
+        self.__c = "GeeksforGeeks"
 
 
+# Creating a derived class
+# class Derived(Base):
+#     def __init__(self):
+#         # Calling constructor of
+#         # Base class
+#         Base.__init__(self)
+#         print("Calling private member of base class: ")
+#         print(self.__c)
+#
+#
+# # Driver code
+# obj1 = Base()
+# print(obj1.a)
 
+from datetime import date
 
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
+        @classmethod
+        def fromBirthYear(cls, name, year):
+            return cls(name, date.today().year - year)
 
+        @staticmethod
+        def isAdult(age):
+            return age > 18
 
+person1 = Person("Rishabh", 21)
+person2 = Person.fromBirthYear('Rishabh', 2002)
 
-# Shahid Kapoor v/s Suriya
-# Ranbir Kapoor v/s Dhanush
-# Hrithik Roshan v/s Thalapathy Vijay
-# Vicky Kaushal v/s Vikram Chiyaan
-# Varun Dhawan  v/s Karthi
+print(person1.age)
+print(person2.age)
+
+print(Person.isAdult(22))
